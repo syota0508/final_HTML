f2a45b9bf543cf502b3046d98ce83a115ff95e4e
syota0508 <syota0508n@gmail.com>
2023-07-23 14:55:16 +0900

    コメントの追加

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-window.addEventListener('load', function () {
-    var $button = document.querySelector('.toggle-menu-button');
-    var $menu = document.querySelector('.header-site-menu');
-    $button.addEventListener('click', function () {
-        if ($menu.classList.contains('is-show')) {
-            $menu.classList.remove('is-show');
+window.addEventListener('load', function () {// 画像の遅延読み込み
+    var $button = document.querySelector('.toggle-menu-button');// ボタン要素
+    var $menu = document.querySelector('.header-site-menu');// メニュー要素
+    $button.addEventListener('click', function () {// ボタンをクリックしたとき
+        if ($menu.classList.contains('is-show')) {// メニューが表示されているとき
+            $menu.classList.remove('is-show');// 表示を解除する
         }
         else {
-            $menu.classList.add('is-show');
+            $menu.classList.add('is-show');// 表示する
         }
     });
 });
 $(function () {
-    $(window).scroll (function () {
-        $("nav.floating").stop().animate(
-            {"top": $(window).scrollTop() + 100},
-        0.000001);
+    $(window).scroll (function () {// スクロールしたとき
+        $("nav.floating").stop().animate(// ナビゲーションを
+            {"top": $(window).scrollTop() + 100},// スクロールに合わせて移動
+        0.000001);// なめらかに移動する
     });
 });
